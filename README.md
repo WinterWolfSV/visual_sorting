e981ee1b05943e1f1e048fea6df8aac46c4fec1a
Theodor Strube <what112222@gmail.com>
2023-01-23 12:24:58 +0100

    Added support to add and generate pillars

--- README.md
+++ README.md
@@ -1,14 +1,34 @@
 # Sorting Algorithm Visualizer
-This program is a tool for visualizing the behavior of different sorting algorithms. It allows users to see how the algorithm sorts an array of values step by step.
+
+This program is a tool for visualizing the behavior of different sorting algorithms. It allows users to see how the
+algorithm sorts an array of values step by step.
 
 ### Visualization of the following sorting algorithms:
-- [ ] Bubble sort 
+
+- [ ] Bubble sort
 - [ ] insertion sort
 - [ ] selection sort
 - [ ] merge sort
 - [ ] quick sort
 - [ ] heap sort
 
-### Additional features
+### Additional features:
+
 - [ ] Adjustable array size and animation speed
 - [ ] Option to randomly shuffle the array before sorting
+
+## Logbook:
+
+---
+
+### 23rd of Jan 2023:
+
+### Done today
+
+- Added support for pillars to be visualized and generated
+  ### TODO
+
+    - Add a slider to select how many pillars that will be generated
+    - Implement one or more of the sorting algorithms named above
+
+---
--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,32 @@
-let sortingArea = document.getElementById("sorting_area");
-
-
+window.onload = main;
 let amountOfSorts = 10;
+const columns = [];
 
-for (let i = 0; i < amountOfSorts; i++) {
-    console.log(i)
-    
-    CreateDiv(i);
+function main() {
+    generateColumns(amountOfSorts);
 }
 
-function CreateDiv(content) {
-    let newDiv = document.createElement("div");
-    let divContent = document.createTextNode(content);
-    newDiv.append(divContent);
 
-    document.body.insertBefore(newDiv, sortingArea);
+function generateColumns(size) {
+    //Creating a loop that adds the pillars an array
+    for (let i = 0; i < size; i++) {
+        let column = document.createElement("div");
 
+        //Sets the classname to "column"
+        column.classList.add("column");
+        //Defines the height and width
+        column.style.width = 100 / size + "%";
+        column.style.height = (i + 1) * (100 / size) + "%";
+        //Adding the div to the sorting area.
+        columns.push(column);
+    }
+    visualizeColumns()
+}
+
+function visualizeColumns() {
+    //Function that visualizes the pillars in the array
+    let sortingArea = document.getElementById("sorting_area");
+    for (let i = 0; i < columns.length; i++) {
+        sortingArea.append(columns[i]);
+    }
 }
